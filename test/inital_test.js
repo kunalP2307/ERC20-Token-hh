c4ba6fa4ba49f50594f87eccc646bb4b25ec781a
kunalP2307 <kunalpatil2307@gmail.com>
2023-02-11 18:19:14 +0530

    added code for other test cases

--- test/inital_test.js
+++ test/inital_test.js
@@ -45,4 +45,60 @@ describe("ERC20 smart contract testing", function(){
         expect (await wallet.balanceOf(accounts[2].address)).to.equal(calculatedVal);
     });
 
+    it("You don't have permission to withdraw ether from contract", async function(){
+        const wallet = token.connect(accounts[2]);
+        await expect(wallet.withdraw(ammount)).to.be.reverted;
+    }); 
+
+    it("transfer ammount to destination account", async function(){
+        await token.transfer(accounts[1].address, ammount);
+        expect(await token.balanceOf(accounts[1].address)).to.equal(ammount); 
+    });
+
+    it("Cannot Transfer above the ammount", async function(){
+        const wallet = token.connect(accounts[3]);
+        await expect(wallet.transfer(accounts[1].address,ammount)).to.be.reverted;
+    });
+
+    it("Cannot Transfer from an empty ammount", async function(){
+        const wallet = token.connect(accounts[3]);
+        await expect(wallet.transfer(accounts[1].address,ammount)).to.be.reverted;
+    });
+
+    it("Test minting token", async function(){
+        const before_mint_bal = await token.balanceOf(accounts[0].address);
+        await token.mint(accounts[0].address, ammount);
+        const after_mint_bal = await token.balanceOf(accounts[0].address);
+        expect (after_mint_bal).to.equal((before_mint_bal.add(ammount)));
+    });
+
+    it("test to burn tokens", async function(){
+        const before_burn_bal = await token.balanceOf(accounts[0].address);
+        await token.burn(accounts[0].address, ammount);
+        const after_burn_bal = await token.balanceOf(accounts[0].address);
+        expect (after_burn_bal).to.equal((before_burn_bal.sub(ammount)));
+    });
+
+    it("test withdraw ether from smart contract",async function(){
+        const before_withdraw_bal = await accounts[0].getBalance();
+        await token.withdraw(ammount);
+        const after_withdraw_bal = await accounts[0].getBalance();
+        expect (before_withdraw_bal.lt(after_withdraw_bal)).to.be.equal(true);
+    });
+
+    it("do not have enough ether to buy token", async function(){
+        const wallet = token.connect(accounts[3]);
+        const big_ammount = ethers.utils.parseEther("999");
+        const option = {value: big_ammount};
+        let error;
+
+        try{
+            await wallet.buy(option);
+        }catch(err){
+            error = "sender does not have enough funds";
+        }
+        
+        expect(error).to.equal("sender does not have enough funds");
+
+    });
 })
